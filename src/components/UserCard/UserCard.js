fb6e9c3db3d904b65be43eeeee0eca2d02fd2ba5
Aidan Kirvan <apkirvan@hotmail.com>
2021-07-30 18:42:20 +1000

    basic styling for UserCard component

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,22 +1,33 @@
 import './UserCard.css'
 
-import { 
+import {
     Grid,
     Paper,
     Button,
+    IconButton,
     ButtonGroup,
-    Typography } from "@material-ui/core"
+    Typography
+} from "@material-ui/core"
+
+import HomeIcon from '@material-ui/icons/Home'
 
 const UserCard = () => {
     return (
         <Grid className="UserCard" item xs={11}>
-            <Paper variant="outlined" style={{ height: 200, width: '100%' }}>
-                <Button>User Profile Link</Button>
-                <Typography variant="h5">Welcome, User</Typography>
-                <ButtonGroup variant="contained" color="secondary">
-                    <Button>Add Pin</Button>
-                    <Button>Manage Pin</Button>
-                </ButtonGroup>
+            <Paper center className="UserCardPaper" variant="outlined" style={{ height: 180, width: '100%', marginBottom: 50 }}>
+                <div className="UserCardPaperHeader" >
+                    <IconButton>
+                        <HomeIcon />
+                    </IconButton>
+                    <Typography variant="h4">Welcome, User</Typography>
+                </div>
+                <div>
+                    <ButtonGroup variant="contained" color="secondary">
+                        <Button>Add Pin</Button>
+                        <Button>Manage Pin</Button>
+                    </ButtonGroup>
+                </div>
+
             </Paper>
         </Grid>
     )
