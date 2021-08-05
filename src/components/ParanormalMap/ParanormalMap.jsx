12e5ec8478a913fd007ee92c995eeebd28875f33
victorzottmann <zottmann.victor@gmail.com>
2021-08-06 02:10:24 +1000

    implement popup display with JSON data when markers are clicked

--- src/components/ParanormalMap/ParanormalMap.jsx
+++ src/components/ParanormalMap/ParanormalMap.jsx
@@ -3,19 +3,18 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './ParanormalMap.css';
-
 import { Grid, Paper, Typography } from '@material-ui/core'; // eslint-disable-line import/no-webpack-loader-syntax
+import geoJson from '../../data/paranormal-locations.json';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
 const ParanormalMap = () => {
   const mapContainer = useRef(null);
-  const map = useRef(null);
 
   const coordinates = {
     sydney: {
-      latitude: -33.855369,
       longitude: 151.208774,
+      latitude: -33.855369,
     },
   };
 
@@ -23,30 +22,96 @@ const ParanormalMap = () => {
   const [lat, setLat] = useState(coordinates.sydney.latitude);
   const [zoom, setZoom] = useState(12);
 
+  // Initialize map when component mounts
   useEffect(() => {
-    if (map.current) {
-      return; // initialize map only once
-    }
-
-    map.current = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/nictordan/ckryudubn0un717pcu6v16nf3',
+      style: 'mapbox://styles/nictordan/ckryyeqgzgcl817mbs5hil5se',
       center: [lng, lat],
-      zoom,
+      zoom: zoom,
     });
-  });
 
-  useEffect(() => {
-    if (!map.current) {
-      return; // wait for map to initialize
-    }
 
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
+    // // Add default markers
+    // geoJson.features.map((feature) =>
+    //   new mapboxgl.Marker()
+    //     .setLngLat(feature.geometry.coordinates)
+    //     .setPopup(popup)
+    //     .addTo(map)
+    // );
+
+    // Add navigation controls
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
     });
-  });
+
+  
+    map.on('load', () => {
+      map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
+        if (error) throw error;
+        map.addImage('custom-marker', image);
+
+        map.addSource('locations', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: geoJson.features,
+          }
+        });
+
+        map.addLayer({
+          id: 'locations',
+          type: 'symbol',
+          source: 'locations',
+          layout: {
+            'icon-image': 'custom-marker',
+            'text-font': ['DIN Offc Pro Italic', 'Arial Unicode MS Regular'],
+          },
+        });
+
+        map.on('click', 'locations', (e) => {
+          const coordinates = e.features[0].geometry.coordinates.slice();
+          const { title, description } = e.features[0].properties
+       
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          // Honestly, I don't understand this math, 
+          // but the docs recommend to include it.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+           
+          new mapboxgl.Popup({ offset: 30 })
+            .setLngLat(coordinates)
+            .setHTML(
+              `<strong>${title}</strong>` +
+              `<p>${description}</p>`
+            )
+            .addTo(map);
+        
+           
+          // Change the cursor to a pointer when the mouse is over the locations layer.
+          map.on('mouseenter', 'locations', function () {
+            map.getCanvas().style.cursor = 'pointer';
+          });
+           
+          // Change it back to a pointer when it leaves.
+          map.on('mouseleave', 'locations', function () {
+            map.getCanvas().style.cursor = '';
+          });
+        });  
+      })
+    })
+
+   
+    // Clean up when components unmounts
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Grid item xs={11}>
