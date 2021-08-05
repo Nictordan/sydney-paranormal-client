7e961ac8cb8a3aa6977b209f24c5489e81371717
victorzottmann <zottmann.victor@gmail.com>
2021-08-06 02:40:47 +1000

    add comments explaining the entire code

--- src/components/ParanormalMap/ParanormalMap.jsx
+++ src/components/ParanormalMap/ParanormalMap.jsx
@@ -6,6 +6,7 @@ import './ParanormalMap.css';
 import { Grid, Paper, Typography } from '@material-ui/core'; // eslint-disable-line import/no-webpack-loader-syntax
 import geoJson from '../../data/paranormal-locations.json';
 
+// Ensure that you have the access token declared in the '.env' file.
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
 const ParanormalMap = () => {
@@ -20,6 +21,8 @@ const ParanormalMap = () => {
 
   const [lng, setLng] = useState(coordinates.sydney.longitude);
   const [lat, setLat] = useState(coordinates.sydney.latitude);
+  // This is the zoom level of the map. The smaller the number, 
+  // the farther away it will be. 
   const [zoom, setZoom] = useState(12);
 
   // Initialize map when component mounts
@@ -31,18 +34,11 @@ const ParanormalMap = () => {
       zoom: zoom,
     });
 
-
-    // // Add default markers
-    // geoJson.features.map((feature) =>
-    //   new mapboxgl.Marker()
-    //     .setLngLat(feature.geometry.coordinates)
-    //     .setPopup(popup)
-    //     .addTo(map)
-    // );
-
     // Add navigation controls
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
+    // Updates state when you move navigate the map. 
+    // toFixed round to x decimal places.
     map.on('move', () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
@@ -51,28 +47,45 @@ const ParanormalMap = () => {
 
   
     map.on('load', () => {
+      // Once the map loads, load an image from this URL (or whatever source we pass in)
       map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
         if (error) throw error;
+
+        // Then add the image with a title.
+        // In this case, 'custom-marker'
         map.addImage('custom-marker', image);
 
+        // Add a source to the map. 
+        // Since it comes from a JSON file with geographic data, 
+        // the type must be 'geojson'.
         map.addSource('locations', {
           type: 'geojson',
           data: {
+            // Every point registered is interpreted as a 'feature'
             type: 'FeatureCollection',
             features: geoJson.features,
           }
         });
 
+        // Then add a layer to the map based on the source added above.
+        // The type here is a symbol because that's how the markers are specified
+        // on Mapbox Studio (at least I suppose it is the reason why)
         map.addLayer({
           id: 'locations',
           type: 'symbol',
           source: 'locations',
+          // Specify what will the icon related to the marked location look like.
+          // In this case, it's that image loaded as 'custom-marker'.
+          // The font family can be customized too apparently.
           layout: {
             'icon-image': 'custom-marker',
             'text-font': ['DIN Offc Pro Italic', 'Arial Unicode MS Regular'],
           },
         });
 
+        // This event listener will fire only when the locations are clicked.
+        // If we click anywhere else in the map, the popups won't show up.
+        // The 'locations' here is the source that we added above.
         map.on('click', 'locations', (e) => {
           const coordinates = e.features[0].geometry.coordinates.slice();
           const { title, description } = e.features[0].properties
@@ -86,6 +99,9 @@ const ParanormalMap = () => {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
           }
            
+          // This offset exists to set the position where the popup with appear.
+          // If set to 0, it will appear right at the marker.
+          // The offset is for the vertical axis. Positive goes up, negative down.
           new mapboxgl.Popup({ offset: 30 })
             .setLngLat(coordinates)
             .setHTML(
@@ -100,7 +116,7 @@ const ParanormalMap = () => {
             map.getCanvas().style.cursor = 'pointer';
           });
            
-          // Change it back to a pointer when it leaves.
+          // Change it back to normal when it leaves the locations layer.
           map.on('mouseleave', 'locations', function () {
             map.getCanvas().style.cursor = '';
           });
@@ -117,6 +133,9 @@ const ParanormalMap = () => {
     <Grid item xs={11}>
       <Paper style={{ width: '100%' }}>
         <Typography variant="h5">Paranormal Activity</Typography>
+
+        {/* This div must contain a 'ref' prop with the mapContainer
+        so that the map gets rendered. */}
         <div ref={mapContainer} className="map-container" />
       </Paper>
     </Grid>
