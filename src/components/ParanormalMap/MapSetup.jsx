5cdef45a02a502b31de78e8f6ddc1cd707e48b20
Aidan Kirvan <apkirvan@hotmail.com>
2021-08-13 20:59:11 +1000

    added workerClass mapbox requirements to MapSetup.jsx

--- src/components/ParanormalMap/MapSetup.jsx
+++ src/components/ParanormalMap/MapSetup.jsx
@@ -8,6 +8,11 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import api from '../../api/api';
 import { Button } from '@material-ui/core';
 
+import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
 
 const mapboxApiKey = process.env.REACT_APP_MAPBOX_KEY;
 
